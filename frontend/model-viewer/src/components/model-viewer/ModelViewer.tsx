354c4c6067b98d5579ef0a9cccea6b3a99db0ef3
Sivan Duijn <sivanduijn@gmail.com>
2022-09-26 14:51:44 +0200

    fix touch rotate

--- frontend/model-viewer/src/components/model-viewer/ModelViewer.tsx
+++ frontend/model-viewer/src/components/model-viewer/ModelViewer.tsx
@@ -17,10 +17,28 @@ export const MemoizedViewGLCanvas = React.memo((props: Props) => {
         props.onMounted(viewGL.current);
     }, []);
 
+    const prevXY = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+
     return (
         <div className={props.className}>
             <canvas
                 ref={canvasRef}
+                onTouchStart={(e) => {
+                    if (e.touches.length != 1) return;
+                    viewGL.current?.onMouseDown();
+                    prevXY.current = { x: e.touches[0].clientX, y: e.touches[0].clientY };
+                }}
+                onTouchMove={(e) => {
+                    if (e.touches.length != 1) return;
+                    const x = e.touches[0].clientX;
+                    const y = e.touches[0].clientY;
+                    viewGL.current?.onMouseDrag(x - prevXY.current.x, y - prevXY.current.y);
+                    prevXY.current = { x, y };
+                }}
+                onTouchEnd={(e) => {
+                    if (e.touches.length != 0) return;
+                    viewGL.current?.onMouseUp();
+                }}
                 onMouseDown={(e) => {
                     if (e.button === 0) {
                         mouseIsDown.current = true;
