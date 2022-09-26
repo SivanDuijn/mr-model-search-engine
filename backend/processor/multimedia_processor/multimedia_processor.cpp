3ef39cbe7dcc67cea3eaa62c379a945df7fce07e
Sivan Duijn <sivanduijn@gmail.com>
2022-09-26 23:18:26 +0200

    test with pmp library

--- backend/processor/multimedia_processor/multimedia_processor.cpp
+++ backend/processor/multimedia_processor/multimedia_processor.cpp
@@ -2,15 +2,24 @@
 //
 
 #include "multimedia_processor.h"
+#include <pmp/SurfaceMesh.h>
 
 using namespace std;
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	if (argc < 2) printf("Please supply an argument");
+	pmp::SurfaceMesh mesh;
+	mesh.read("../../frontend/model-viewer/public/models/animal/m0.obj");
+	cout << "# vertices: " << mesh.n_vertices() << endl;
+	cout << "# faces: " << mesh.n_faces() << endl;
+
+	if (argc < 2)
+		printf("Please supply an argument");
 	// No switch for "strings" :(
-	else if (!strcmp(argv[1], "debug")) debug();
-	else printf("Unknown argument");
+	else if (!strcmp(argv[1], "debug"))
+		debug();
+	else
+		printf("Unknown argument");
 
 	return 0;
 }
