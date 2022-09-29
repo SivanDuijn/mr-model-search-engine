566cf9ec75f7b1e31f9e09a8e6a78bbc6d511a94
Sivan Duijn <sivanduijn@gmail.com>
2022-09-29 13:17:58 +0200

    ja man

--- frontend/model-viewer/src/lib/getModelStats.ts
+++ frontend/model-viewer/src/lib/getModelStats.ts
@@ -1,21 +1,20 @@
 import ModelInfo from "../data/model-info.json";
 
 export interface ModelStats {
-    className: string;
+    className?: string;
     nFaces?: number;
     nVertices?: number;
-    AABB: { min: { x: number; y: number; z: number }; max: { x: number; y: number; z: number } };
+    AABB?: { min: { x: number; y: number; z: number }; max: { x: number; y: number; z: number } };
 }
 
 export default function GetModelStats(
     modelFileName: string,
     mesh: THREE.Mesh,
 ): ModelStats | undefined {
-    if (!(modelFileName in ModelInfo)) return undefined;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const info = (ModelInfo as any)[modelFileName];
     return {
-        className: info[0],
+        className: info ? info[0] : undefined,
         nVertices: mesh.geometry.getAttribute("position").count,
         nFaces: mesh.geometry.index ? mesh.geometry.index.count / 3 : undefined,
         AABB: {
