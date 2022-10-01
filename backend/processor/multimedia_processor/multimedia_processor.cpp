ae8da280546531ec2f9d19e3d0a6688c0d456226
unknown <v.l.vanzwieten@students.uu.nl>
2022-10-01 18:04:37 +0200

    Added moment test

--- backend/processor/multimedia_processor/multimedia_processor.cpp
+++ backend/processor/multimedia_processor/multimedia_processor.cpp
@@ -80,7 +80,10 @@ void normalize(const char* path)
 	printf("Loading mesh \"%s\"\n", path);
 	pmp::SurfaceMesh mesh;
 	mesh.read(vars::GetAssetPath(path));
+	// Map the point list (aka array of Eigen::Matrix3f) to a single Eigen::MatrixXd
+	// TODO utilize more
 	pmp::VertexProperty points = mesh.get_vertex_property<pmp::Point>("v:point");
+	Eigen::Map<Eigen::MatrixXf> map((float*)(points.data()), 3, mesh.n_vertices());
 
 	// Translate barycenter to origin
 	printf("Translating barycenter to origin");
@@ -90,8 +93,6 @@ void normalize(const char* path)
 
 	// Align with coordinate frame
 	printf("Aligning with coordinate frame");
-	// Map the point list (aka array of Eigen::Matrix3f) to a single Eigen::MatrixXd
-	Eigen::Map<Eigen::MatrixXf> map((float*)(points.data()), 3, mesh.n_vertices());
 	// Compute the covariance matrix
 	// https://stackoverflow.com/a/15142446
 	// We don't have to center the sample matrix because we just centered the samples :)
@@ -102,7 +103,7 @@ void normalize(const char* path)
 	Eigen::MatrixXf eigen = solver.eigenvectors().real();
 	// Rotate the model
 	// TODO optimize code
-	Eigen::Vector3f major = eigen.row(0);
+	Eigen::Vector3f	 major = eigen.row(0);
 	Eigen::Vector3f minor = eigen.row(1);
 	Eigen::Vector3f cross = major.cross(minor);
 	Eigen::Matrix3f rot; rot << -major, -minor, -cross;
@@ -110,7 +111,10 @@ void normalize(const char* path)
 	printf(" ([%f, %f, %f] / [%f, %f, %f] / [%f, %f, %f])\n", major(0), major(1), major(2), minor(0), minor(1), minor(2), cross(0), cross(1), cross(2));
 
 	// Flip based on moment test
-	// TODO
+	printf("Flipping based on moment test");
+	Eigen::Vector3f flip = map.cwiseSign().cwiseProduct(map.cwiseProduct(map)).rowwise().sum().cwiseSign();
+	for (auto v : mesh.vertices()) points[v] = flip.cwiseProduct((Eigen::Vector3f)points[v]);
+	printf(" [%f, %f, %f]\n", flip(0), flip(1), flip(2));
 
 	// Scale to unit volume
 	printf("Scaling to unit volume");
