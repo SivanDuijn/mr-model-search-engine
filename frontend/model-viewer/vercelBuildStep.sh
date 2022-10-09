7df43c849a1df6acfb461a3795e92d079d42d51a
Sivan Duijn <sivanduijn@gmail.com>
2022-10-09 09:24:20 +0200

    ignore other branches vercel build step

---
+++ frontend/model-viewer/vercelBuildStep.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+  
+  if git diff HEAD^ HEAD --quiet . ; then
+    echo "✅ - Build can proceed"
+    exit 1;
+  else
+    echo "🛑 - Build cancelled, frontend not changed"
+  fi
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled, wrong branch"
+  exit 0;
+fi
+
