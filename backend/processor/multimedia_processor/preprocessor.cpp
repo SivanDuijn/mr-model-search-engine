5430ee3c951077826a1582a1772120a5804fe7c4
unknown <v.l.vanzwieten@students.uu.nl>
2022-10-12 14:29:35 +0200

    Some cleanup

--- backend/processor/multimedia_processor/preprocessor.cpp
+++ backend/processor/multimedia_processor/preprocessor.cpp
@@ -9,7 +9,7 @@ namespace preprocessor
     {
         printf_debug("Mesh has %zu vertices\n", mesh.n_vertices());
 
-        // Store stats before resampling
+        // Store vertices and faces before resampling
         beforeStats.nVertices = mesh.n_vertices();
         beforeStats.nFaces = mesh.n_faces();
 
@@ -42,7 +42,7 @@ namespace preprocessor
             printf_debug("Mesh contains exactly the right amount of vertices!\n");
         }
 
-        // Store stats after resampling
+        // Store vertices and faces after resampling
         afterStats.nVertices = mesh.n_vertices();
         afterStats.nFaces = mesh.n_faces();
 
@@ -60,14 +60,17 @@ namespace preprocessor
         pmp::VertexProperty points = mesh.get_vertex_property<pmp::Point>("v:point");
         Eigen::Map<Eigen::MatrixXf> map((float*)(points.data()), 3, mesh.n_vertices());
 
-        // Translate barycenter to origin
+        // Store barycenter distance to origin before translation
         beforeStats.distBarycenterToOrigin = ((Eigen::Vector3f)pmp::centroid(mesh)).norm();
+
+        // Translate barycenter to origin
         printf_debug("Translating barycenter to origin");
         pmp::Point bcenter = pmp::centroid(mesh);
         map.colwise() -= (Eigen::Vector3f)bcenter;
         printf_debug(" (-[%f, %f, %f])\n", bcenter.data()[0], bcenter.data()[1], bcenter.data()[2]);
-        afterStats.distBarycenterToOrigin = ((Eigen::Vector3f)pmp::centroid(mesh)).norm();
 
+        // Store barycenter distance to origin after translation
+        afterStats.distBarycenterToOrigin = ((Eigen::Vector3f)pmp::centroid(mesh)).norm();
 
         // Align with coordinate frame
         printf_debug("Aligning with coordinate frame\n");
@@ -78,22 +81,26 @@ namespace preprocessor
         Eigen::Matrix3f cov = (map * map.adjoint()) / float(map.cols() - 1);
         auto eigenVs = utils::GetMajorMinorEigenVectors(cov);
         Eigen::Vector3f	major = get<0>(eigenVs);
-        Eigen::Vector3f minor = get<1>(eigenVs);
-        Eigen::Vector3f cross = get<2>(eigenVs);
+        Eigen::Vector3f middl = get<1>(eigenVs);
+        Eigen::Vector3f minor = get<2>(eigenVs);
         // Create the inverse rotation matrix by transposing the eigenvectors to it
         Eigen::Matrix3f rot {
             {major(0), major(1), major(2)},
+            {middl(0), middl(1), middl(2)},
             {minor(0), minor(1), minor(2)},
-            {cross(0), cross(1), cross(2)},
         };
-        // Store stats before rotating
+
+        // Store rotation before rotating
         beforeStats.angleX = pmp::angle(major, pmp::Point(1,0,0));
-        beforeStats.angleY = pmp::angle(minor, pmp::Point(0,1,0));
-        beforeStats.angleZ = pmp::angle(cross, pmp::Point(0,0,1));
+        beforeStats.angleY = pmp::angle(middl, pmp::Point(0,1,0));
+        beforeStats.angleZ = pmp::angle(minor, pmp::Point(0,0,1));
         beforeStats.totalAngle = beforeStats.angleX + beforeStats.angleY + beforeStats.angleZ;
+
         // Rotate the model
         for (auto v : mesh.vertices()) points[v] = rot * (Eigen::Vector3f)(points[v]);
-        // and store stats after rotating
+        printf_debug(" ([%f, %f, %f])\n", -beforeStats.angleX, -beforeStats.angleY, -beforeStats.angleZ);
+
+        // Store rotation after rotating
         cov = (map * map.adjoint()) / float(map.cols() - 1);
         eigenVs = utils::GetMajorMinorEigenVectors(cov);
         afterStats.angleX = pmp::angle(get<0>(eigenVs), pmp::Point(1,0,0));
@@ -105,27 +112,38 @@ namespace preprocessor
         printf_debug("Flipping based on moment test");
         Eigen::Vector3f flip = map.cwiseSign().cwiseProduct(map.cwiseProduct(map)).rowwise().sum().cwiseSign()
             .unaryExpr([](float e) -> float { return e == 0.f ? 1.f : e; }); // Change all 0s to 1s
-        // Store before stats
+
+        // Store flip before flipping
         beforeStats.totalFlip = flip.sum();
-        printf_debug(" [%f, %f, %f]\n", flip(0), flip(1), flip(2));
+
+        // Flip the model
         for (auto v : mesh.vertices()) points[v] = ((Eigen::Vector3f)points[v]).cwiseProduct(flip); // TODO Eigen calculations
-        // Store after stats
+        printf_debug(" ([%f, %f, %f])\n", flip(0), flip(1), flip(2));
+
+        // Store flip after flipping
         Eigen::Vector3f afterFlip = map.cwiseSign().cwiseProduct(map.cwiseProduct(map)).rowwise().sum().cwiseSign()
-            .unaryExpr([](float e) -> float { return e == 0.f ? 1.f : e; });
+            .unaryExpr([](float e) -> float { return e == 0.f ? 1.f : e; }); // Change all 0s to 1s
         afterStats.totalFlip = afterFlip.sum();
 
         // Scale to unit volume
-        beforeStats.boundingBoxSize = mesh.bounds().size();
         printf_debug("Scaling to unit volume");
         Eigen::MatrixXf bounds = Eigen::MatrixXf(3, 2);
         bounds << (Eigen::Vector3f)(mesh.bounds().min()), (Eigen::Vector3f)(mesh.bounds().max());
         float max = bounds.cwiseAbs().maxCoeff();
         float scale = 0.5f / max;
+
+        // Store size before scaling
+        beforeStats.boundingBoxSize = mesh.bounds().size();
+
+        // Scale the model
         map *= scale;
         printf_debug(" (%f => *%f)\n", max, scale);
+
+        // Store size after scaling
         afterStats.boundingBoxSize = mesh.bounds().size();
 
         // If the mesh was flipped once or thrice we need to invert the normals
+        // TODO optimize (in place?)
         int flipSum = flip.sum();
         if (flipSum == 1 || flipSum == -3)
         {
@@ -145,5 +163,4 @@ namespace preprocessor
         
         printf_debug("Done\n");
     }
-
 }
