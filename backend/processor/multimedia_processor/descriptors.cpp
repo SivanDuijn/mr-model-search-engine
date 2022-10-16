72668aec3107e858ed76759ed77b706714b05b25
unknown <v.l.vanzwieten@students.uu.nl>
2022-10-16 13:07:14 +0200

    Work on D3

--- backend/processor/multimedia_processor/descriptors.cpp
+++ backend/processor/multimedia_processor/descriptors.cpp
@@ -47,4 +47,35 @@ namespace descriptors
         // Bin the result
         return bin(dist, bins);
     }
+
+    Eigen::VectorXi D3(pmp::SurfaceMesh &mesh, int bins)
+    {
+        return D2(mesh, bins);
+
+        /*
+        // Get the vertices as an Eigen map
+        Eigen::Map<Eigen::MatrixXf> map = utils::GetVertexMap(mesh);
+
+        // Get all the areas of random triangles
+        Eigen::MatrixXf verts1 = Eigen::MatrixXf(3, VERTEX_COUNT);
+        Eigen::MatrixXf verts2 = Eigen::MatrixXf(3, VERTEX_COUNT);
+        for (size_t i = 0; i < VERTEX_COUNT; i++) 
+        {
+            verts1.col(i) = map.col(utils::RandomVertexIndex());
+            verts2.col(i) = map.col(utils::RandomVertexIndex());
+        }
+        verts1 -= map;
+        verts2 -= map;
+        // No colwise cross :(
+        Eigen::VectorXf area = Eigen::VectorXf(VERTEX_COUNT);
+        for (size_t i = 0; i < VERTEX_COUNT; i++) 
+        {
+            Eigen::Vector3f test = verts1.col(i).cross(verts2.col(i));
+            area[i] = test.norm() / 2;
+        }
+
+        // Bin the result
+        return bin(area, bins);
+        */
+    }
 }
