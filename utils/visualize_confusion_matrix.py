e7b9c8c8a8f4c0804f32dd177484613de7856758
Sivan Duijn <sivanduijn@gmail.com>
2022-11-08 19:14:44 +0100

    add visualize confusion matrix script

---
+++ utils/visualize_confusion_matrix.py
@@ -0,0 +1,14 @@
+import json
+import seaborn as sn
+import pandas as pd
+import matplotlib.pyplot as plt
+import sys
+
+f = open("../frontend/model-viewer/public/PSBDatabase/confusion_matrix.json")
+confusion = json.load(f)
+
+df_cm = pd.DataFrame(confusion["matrix"], confusion["class_names"],
+        confusion["class_names"])
+plt.figure(figsize = (10,7))
+sn.heatmap(df_cm, annot=True, cmap="Reds", fmt='.3g')
+plt.show()
