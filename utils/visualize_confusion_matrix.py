c4f952838016116768679b3854b6f959286617e4
Sivan Duijn <sivanduijn@gmail.com>
2022-11-20 16:54:01 +0100

    remove Debug models from database

--- utils/visualize_confusion_matrix.py
+++ utils/visualize_confusion_matrix.py
@@ -6,9 +6,21 @@ import sys
 
 f = open("../frontend/model-viewer/public/PSBDatabase/confusion_matrix.json")
 confusion = json.load(f)
+# confusion["matrix"] = map(lambda l : map(lambda x : x / 200, l), confusion["matrix"])
+
+out = ""
+i = 0
+for l in confusion["matrix"]:
+    out += confusion["class_names"][i]
+    for v in l:
+        out += " & {:.3g}".format(v / 200)
+    out += "\\\\\n\\hline\n"
+    i += 1
+
+print(out)
 
 df_cm = pd.DataFrame(confusion["matrix"], confusion["class_names"],
         confusion["class_names"])
 plt.figure(figsize = (10,7))
-sn.heatmap(df_cm, annot=True, cmap="Blues", fmt='.3g')
+sn.heatmap(df_cm, annot=True, cmap="Blues", fmt='g')
 plt.show()
